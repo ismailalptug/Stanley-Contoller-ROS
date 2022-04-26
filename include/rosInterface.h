f2495187ab26a46c88b78786f5f0581b2819028e
ismailalptug <alptugismaill@gmail.com>
2022-04-26 17:30:11 +0300

    Ros Interface has been added as a class

--- include/rosInterface.h
+++ include/rosInterface.h
@@ -1,23 +1,33 @@
 #include "ros/ros.h"
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float32MultiArray.h>
+#include "stanley.h"
 
-class ros_interface
+class rosInterface
 {
 
 private:
-    ros::Publisher pub_;
+
+    ros::NodeHandle &n;
+    ros::Publisher pub_1;
     ros::Subscriber sub_1, sub_2;
+    stanleyController stanley;
+    float vel;
+    float arr[2];
+    void getVelocityCallback(const std_msgs::Float32::ConstPtr& velocity);
+    void getErrorCallback(const std_msgs::Float32MultiArray::ConstPtr& error);
+  
 
 
 
 public:
-    //void getVelocityCallback(const std_msgs::Float32::ConstPtr& velocity);
-    //void getErrorCallback(const std_msgs::Float32MultiArray::ConstPtr& error);
+
     void execute_ros();
     void init_ros();
-    void deinit_ros();
-    float vel;
-    float arr[2];
+ 
+
+    rosInterface(ros::NodeHandle &n); 
+    
+    
 
 };
--- src/stanley_node.cpp
+++ src/stanley_node.cpp
@@ -1,68 +1,19 @@
 #include "stanley.h"
 #include "rosInterface.h"
-//#include "ros/ros.h"
 
 
-
-/*
-float vel;
-float arr[2];
-
-void getVelocityCallback(const std_msgs::Float32::ConstPtr& velocity)
-{   
-    vel = velocity->data;
-    ROS_INFO("I heard: [%f]", velocity->data);
-}
-
-void getErrorCallback(const std_msgs::Float32MultiArray::ConstPtr& error)
-{
-    arr[0] = error->data.at(0);
-    arr[1] = error->data.at(1);
-    ROS_INFO("I heard: [%f], [%f]", error->data.at(0),error->data.at(1));
-}
-*/
 
 int main(int argc, char **argv) 
 {
-   
-    stanleyController stanley;
-    ros_interface myRos;
-
     ros::init(argc, argv, "Stanley_Controller_Node");
-    //myRos.init_ros();
+
+    
+    ros::NodeHandle n;
+    rosInterface myRos(n);
+
+    myRos.init_ros();
     myRos.execute_ros();
 
-    /*
-    ros::init(argc, argv, "Stanley_Contoller_Node");  
-    ros::NodeHandle n;
-    ros::Subscriber sub1 = n.subscribe("vehicle_status", 1000, getVelocityCallback);
-    ros::Subscriber sub2 = n.subscribe("desired_set_points", 1000, getErrorCallback);
-    ros::Publisher pub1 = n.advertise<std_msgs::Float32>("desired_control", 1000);
-    ros::Rate loop_rate(20); 
-    */
-    
-
-    /*
-    while (ros::ok())
-    {
-        std_msgs::Float32 steering;
-        
-        stanley.setInputError(arr[0], arr[1]);
-        stanley.setInputVelocity(vel);
-        stanley.execute();
-
-
-        steering.data = stanley.getOutput();
-        ROS_INFO("%f", steering.data);
-        pub1.publish(steering);
-
-
-        ros::spinOnce();
-        loop_rate.sleep();
-
-
-    }
-    */
 
 
 
